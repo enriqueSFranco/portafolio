23895e38fe05fadc0f59ac84f03b325bcbc10600
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-11-14 13:17:32 -0600

    chore: se eliminaron los icons: zustand, django, twitch, styled components y tip-tap

---
+++ src/shared/constants.d.ts
@@ -0,0 +1,83 @@
+import pythonIcon from "/icons/python.svg"
+import jsIcon from "/icons/javascript.svg"
+import tsIcon from "/icons/typescript.svg"
+import reactIcon from "/icons/react.svg"
+import NextjsIcon from "/icons/nextjs.svg"
+import nodeIcon from "/icons/node.svg"
+import tailwindIcon from "/icons/tailwindcss.svg"
+import postgresqlIcon from "/icons/postgresql.svg"
+import gitIcon from "/icons/git.svg"
+
+
+export const TAGS = {
+  JAVASCRIPT: {
+    name: "JavaScript",
+    class: "bg-yellow-500/20 text-black",
+    icon: jsIcon,
+  },
+  TYPESCRIPT: {
+    name: "Typescript",
+    class: "bg-blue-500/20 text-black",
+    icon: tsIcon,
+  },
+  REACT: {
+    name: "React",
+    class: "bg-blue-300/20 text-black",
+    icon: reactIcon,
+  },
+  NEXTJS: {
+    name: "Nextjs",
+    class: "bg-black outline outline-[1px] outline-white/20 text-black",
+    icon: NextjsIcon,
+  },
+  TAILWINDCSS: {
+    name: "TailwindCss",
+    class: "bg-teal-500/20 text-black",
+    icon: tailwindIcon,
+  },
+  PYTHON: {
+    name: "Python",
+    class: "bg-blue-500/20 text-black",
+    icon: pythonIcon,
+  },
+  POSTGRESQL: {
+    name: "Postgresql",
+    class: "bg-blue-500/40 text-black",
+    icon: postgresqlIcon,
+  },
+  NODE: {
+    name: "Node.js",
+    class: "bg-emerald-500/20 text-black",
+    icon: nodeIcon,
+  },
+  GIT: {
+    name: "Git",
+    class: "bg-orange-500/40 text-black",
+    icon: gitIcon,
+  },
+  RECOMMENDATION_ALGORITHM: {
+    name: "Recommendation Algorithm",
+    class: "bg-gray-100 text-black",
+    icon: null,
+  },
+  PUPPETEER: {
+    name: "Puppeteer",
+    class: "bg-emeralda-500 text-black",
+    icon: null,
+  },
+  API_REST: {
+    name: "Api REST",
+    class: "bg-gray-100 text-black",
+    icon: null,
+  },
+  WEB_SCRAPING: {
+    name: "Web Scraping",
+    class: "bg-gray-100 text-black",
+    icon: null,
+  },
+  DJANGO_REST_FRAMEWORK: {
+    name: "Dejango REST Framework",
+    class: "bg-[#a30100] text-black",
+    icon: null,
+  }
+}
