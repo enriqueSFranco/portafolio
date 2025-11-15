091351124a552381ab1da75f8fa010b9f8be1946
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-11-15 15:46:37 -0600

    feat: configuracion para el dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,50 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			animation: {
-				'say': 'say 2.5s infinite'
-			},
-			keyframes: {
-				say: {
-					'0%': {
-						transform: 'rotate(0deg)'
-					},
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        say: "say 2.5s infinite",
+      },
+      keyframes: {
+        say: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
 
-					'10%': {
-						transform: 'rotate(14deg)'
-					},
+          "10%": {
+            transform: "rotate(14deg)",
+          },
 
-					'20%': {
-						transform: 'rotate(-8deg)'
-					},
+          "20%": {
+            transform: "rotate(-8deg)",
+          },
 
-					'30%': {
-						transform: 'rotate(14deg)'
-					},
+          "30%": {
+            transform: "rotate(14deg)",
+          },
 
-					'40%': {
-						transform: 'rotate(-4deg)'
-					},
+          "40%": {
+            transform: "rotate(-4deg)",
+          },
 
-					'50%': {
-						transform: 'rotate(10deg)'
-					},
+          "50%": {
+            transform: "rotate(10deg)",
+          },
 
-					'60%': {
-						transform: 'rotate(0deg)'
-					},
+          "60%": {
+            transform: "rotate(0deg)",
+          },
 
-					'100%': {
-						transform: 'rotate(0deg)'
-					}
-				}
-			},
-			fontFamily: {
-				inter: "'Inter', sans-serif"
-			}
-		},
-	},
-	plugins: [],
-}
+          "100%": {
+            transform: "rotate(0deg)",
+          },
+        },
+      },
+      fontFamily: {
+        inter: "'Inter', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
