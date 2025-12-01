0b9048a633f4b548d73b598c76f5662f5ecdbd5d
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-12-01 17:24:36 -0600

    chore: se elimino la animacion de say

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,48 +4,8 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      animation: {
-        say: "say 2.5s infinite",
-      },
-      keyframes: {
-        say: {
-          "0%": {
-            transform: "rotate(0deg)",
-          },
-
-          "10%": {
-            transform: "rotate(14deg)",
-          },
-
-          "20%": {
-            transform: "rotate(-8deg)",
-          },
-
-          "30%": {
-            transform: "rotate(14deg)",
-          },
-
-          "40%": {
-            transform: "rotate(-4deg)",
-          },
-
-          "50%": {
-            transform: "rotate(10deg)",
-          },
-
-          "60%": {
-            transform: "rotate(0deg)",
-          },
-
-          "100%": {
-            transform: "rotate(0deg)",
-          },
-        },
-      },
-      fontFamily: {
-        inter: "'Inter', sans-serif",
-      },
-    },
+      
+    }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
