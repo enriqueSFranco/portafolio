4155d7293253b35476b3ed305876bb358460ea6d
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-12-01 17:24:24 -0600

    configuracion para svg y plugin de tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  markdown: {
-    shikiConfig: {
-      theme: 'dracula',
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    svgo: true,
   },
 });
