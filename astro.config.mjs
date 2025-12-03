8aacd3f29acd9b07ba630b3a54c6352dee0c5044
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-12-02 18:43:02 -0600

    config: se agrego configuracion para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://enriqueSFranco.github.io",
+  base: "enriqueSF",
   vite: {
     plugins: [tailwindcss()],
   },
