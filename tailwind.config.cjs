deb6dd8175d1c0d0cc685a9fb0883117dd590c75
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-12-02 18:42:44 -0600

    configuracion de tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,5 @@
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      
-    }
-  },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/typography")],
 };
