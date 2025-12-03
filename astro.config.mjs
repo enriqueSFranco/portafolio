7f3560cd49ab21d364cc9abd92a91e7aeb866226
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-12-03 17:47:43 -0600

    se cambio la ruta de github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://enriqueSFranco.github.io",
-  base: "/mi-portafolio/",
+  base: "/portafolio/",
   vite: {
     plugins: [tailwindcss()],
   },
